459f0f875babbeb70e8af677db539744b6be7a74
Rajesh <rajeshshaw.abc@gmail.com>
2023-01-16 18:14:04 +0530

    Get user details using wire adaptor

--- force-app/main/default/lwc/wireUserDetail/wireUserDetail.js
+++ force-app/main/default/lwc/wireUserDetail/wireUserDetail.js
@@ -1,9 +1,9 @@
-import { LightningElement,wire} from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import Id from '@salesforce/user/Id';
 import EMAIL_FIELD from '@salesforce/schema/User.Email';
 import NAME_FIELD from '@salesforce/schema/User.Name';
-const userFields = [EMAIL_FIELD,NAME_FIELD];
+const userFields = [EMAIL_FIELD, NAME_FIELD];
 export default class WireUserDetail extends LightningElement {
     userId = Id;
     userDetail;
@@ -11,20 +11,19 @@ export default class WireUserDetail extends LightningElement {
     // @wire(adapter,{adapterConfig})
     // propertyorfunction
 
-   
-    @wire(getRecord,{recordId:'0051m000004XZSkAAO',fields:userFields})
-    userDetailHandler({data,error})
-    {
-       if(data)
-       {
-        this.userDetail = data.fields;
-        console.log(this.userDetail);
-       }
-       if(error)
-       {
-        console.error(error);
-       }
+    //IF you use {recordId:userId} then it will throw error beacuse import id is asynchronous operation and @wire is called before import statement so 
+    //id is not available at that time and it throws error. So to make the @wire reactive we use $ symbol {recordId:'$userId'} , 
+    //so when the userId is available again it loads the @wire and shows the result, Conclusion use $symbol with {recordId:'$userId'}
+    @wire(getRecord, { recordId: '$userId', fields: userFields }) // OR @wire(getRecord,{recordId:'0051m000004XZSkAAO',fields:userFields})
+    userDetailHandler({ data, error }) {
+        if (data) {
+            this.userDetail = data.fields;
+            console.log(this.userDetail);
+        }
+        if (error) {
+            console.error(error);
+        }
     }
-    @wire(getRecord,{recordId:'0051m000004XZSkAAO',fields:userFields})
+    @wire(getRecord, { recordId: '$userId', fields: userFields })
     userDetailProperty;
 }
